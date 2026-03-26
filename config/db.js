9ab642a5416f3fb5a30317dcde9f7b9ad9711a97
Charan Kumar <kanna.charan2006@gmail.com>
2026-03-26 11:40:40 +0530

    updated data base connection

--- config/db.js
+++ config/db.js
@@ -1,11 +1,11 @@
 const mysql = require('mysql2');
 
 const db = mysql.createPool({
-    host: process.env.DB_HOST || 'localhost',
-    user: process.env.DB_USER || 'root',
-    password: process.env.DB_PASSWORD || 'Charan2006$',
-    database: process.env.DB_NAME || 'seafood_erp',
-    port: process.env.DB_PORT || 3306,
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT,
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0,
